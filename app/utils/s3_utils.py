6f276f1d5c52ee8c58bf8639e94e0bc3a40c5945
CHO Min Ho <chominho96@naver.com>
2024-08-28 20:56:59 +0900

    fix: fix s3 util to get image as input

--- app/utils/s3_utils.py
+++ app/utils/s3_utils.py
@@ -1,6 +1,9 @@
 import boto3
 from botocore.exceptions import NoCredentialsError, ClientError
 from app.core.settings import settings
+from PIL import Image
+from io import BytesIO
+import uuid
 
 class S3Service:
     def __init__(self):
@@ -10,13 +13,21 @@ class S3Service:
             aws_secret_access_key=settings.AWS_SECRET_KEY
         )
 
-    def upload_file(self, file_name: str, bucket: str = settings.AWS_S3_BUCKET, object_name=None):
+    def upload_image_to_s3(self, image: Image.Image, bucket: str = settings.AWS_S3_BUCKET, object_name=None, format='JPEG'):
         try:
+            # 메모리에 이미지를 저장한 후, 이를 s3에 업로드
+            buffer = BytesIO()
+            image.save(buffer, format=format)
+            buffer.seek(0)
+
             if object_name is None:
-                object_name = file_name
-            self.s3_client.upload_file(file_name, bucket, object_name)
+                object_name = str(uuid.uuid1()) + format.lower()
+
+            # s3에 업로드
+            self.s3_client.upload_fileobj(buffer, bucket, object_name)
+
             return {"status": "success", "object_name": object_name}
         except NoCredentialsError:
             return {"status": "error", "message": "Credentials not available"}
-        except ClientError as e:
+        except Exception as e:
             return {"status": "error", "message": str(e)}
