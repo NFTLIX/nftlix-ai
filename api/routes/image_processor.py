a3c8767f0b7e9593c3b8dd76d055b17ea1cfe1cc
CHO Min Ho <chominho96@naver.com>
2024-09-03 22:08:29 +0900

    feat: add nukki api

--- api/routes/image_processor.py
+++ api/routes/image_processor.py
@@ -1,13 +1,16 @@
 from fastapi import APIRouter, UploadFile, File
 from services.cartoonization import CartoonizationService
 from services.black_and_white import BlackAndWhiteService
+from services.nukki import NukkiService
 from PIL import Image
 import io
+import os
 
 router = APIRouter()
 
 cartoonization_service = CartoonizationService()
 black_and_white_service = BlackAndWhiteService()
+nukki_service = NukkiService()
 
 @router.post("/cartoonization/")
 async def cartoonize(image: UploadFile = File(...)):
@@ -17,8 +20,22 @@ async def cartoonize(image: UploadFile = File(...)):
     return cartoonization_service.convert(image, image_name)
 
 @router.post("/black-and-white/")
-async def cartoonize(image: UploadFile = File(...)):
+async def black_and_white(image: UploadFile = File(...)):
     image_name = image.filename
     image_bytes = await image.read()
     image = Image.open(io.BytesIO(image_bytes))
     return black_and_white_service.convert(image, image_name)
+
+@router.post("/nukki/")
+async def nukki(image: UploadFile = File(...)):
+    image_name = image.filename
+    image_bytes = await image.read()
+    image = Image.open(io.BytesIO(image_bytes))
+
+    # 누끼는 png 확장자에 대해서만 동작
+    if image.format != 'PNG':
+        base, _ = os.path.splitext(image_name)
+        new_name = f"{base}.png"
+        image.save(new_name, 'png')
+
+    return nukki_service.convert(image, image_name)
