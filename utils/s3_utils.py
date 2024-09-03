2519d12315a4faecc30875085e47ed6b26b7a731
CHO Min Ho <chominho96@naver.com>
2024-09-03 22:47:14 +0900

    feat: add json upload method

--- utils/s3_utils.py
+++ utils/s3_utils.py
@@ -8,7 +8,7 @@ import uuid
 class S3Service:
     def __init__(self):
         self.s3_client = boto3.client(
-            's3',
+            's3',\
             aws_access_key_id=settings.AWS_ACCESS_KEY,
             aws_secret_access_key=settings.AWS_SECRET_KEY
         )
@@ -28,6 +28,18 @@ class S3Service:
             self.s3_client.upload_fileobj(buffer, bucket, image_path)
 
             return {"status": "success", "url": f"https://{self.bucket_name}.s3.amazonaws.com/{image_path}"}
+        except NoCredentialsError:
+            return {"status": "error", "message": "Credentials not available"}
+        except Exception as e:
+            return {"status": "error", "message": str(e)}
+
+    def upload_metadata_to_s3(self, json_buffer, bucket: str = settings.AWS_S3_METADATA_BUCKET):
+        try:
+            # TODO: metadata 이름을 특정 이름으로 수정해야할 필요 있을 시 수정
+            metadata_name = str(uuid.uuid1()) + '.' + 'json'
+            self.s3_client.upload_fileobj(json_buffer, bucket, metadata_name)
+
+            return {"status": "success", "url": f"https://{bucket}.s3.amazonaws.com/{metadata_name}"}
         except NoCredentialsError:
             return {"status": "error", "message": "Credentials not available"}
         except Exception as e:
