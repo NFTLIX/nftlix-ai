82e906d5d9ed3fd456e2ca41b0335c70501b379d
CHO Min Ho <chominho96@naver.com>
2024-09-03 21:53:05 +0900

    feat: add black and white api

--- api/routes/image_processor.py
+++ api/routes/image_processor.py
@@ -1,11 +1,13 @@
-from fastapi import APIRouter, UploadFile, File, HTTPException
+from fastapi import APIRouter, UploadFile, File
 from services.cartoonization import CartoonizationService
+from services.black_and_white import BlackAndWhiteService
 from PIL import Image
 import io
 
 router = APIRouter()
 
 cartoonization_service = CartoonizationService()
+black_and_white_service = BlackAndWhiteService()
 
 @router.post("/cartoonization/")
 async def cartoonize(image: UploadFile = File(...)):
@@ -14,3 +16,9 @@ async def cartoonize(image: UploadFile = File(...)):
     image = Image.open(io.BytesIO(image_bytes))
     return cartoonization_service.convert(image, image_name)
 
+@router.post("/black-and-white/")
+async def cartoonize(image: UploadFile = File(...)):
+    image_name = image.filename
+    image_bytes = await image.read()
+    image = Image.open(io.BytesIO(image_bytes))
+    return black_and_white_service.convert(image, image_name)
