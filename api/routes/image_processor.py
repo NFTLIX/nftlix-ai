3b16df0f5ef8e53f5e6e80e2c1ca53b5381a0e0f
CHO Min Ho <chominho96@naver.com>
2024-09-03 22:21:36 +0900

    fix: fix nukki png convert logic

--- api/routes/image_processor.py
+++ api/routes/image_processor.py
@@ -4,7 +4,6 @@ from services.black_and_white import BlackAndWhiteService
 from services.nukki import NukkiService
 from PIL import Image
 import io
-import os
 
 router = APIRouter()
 
@@ -31,11 +30,4 @@ async def nukki(image: UploadFile = File(...)):
     image_name = image.filename
     image_bytes = await image.read()
     image = Image.open(io.BytesIO(image_bytes))
-
-    # 누끼는 png 확장자에 대해서만 동작
-    if image.format != 'PNG':
-        base, _ = os.path.splitext(image_name)
-        new_name = f"{base}.png"
-        image.save(new_name, 'png')
-
     return nukki_service.convert(image, image_name)
