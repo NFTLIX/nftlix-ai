f556d785bd712d06493fb619ff2faeb62bd4b551
CHO Min Ho <chominho96@naver.com>
2024-09-03 22:08:17 +0900

    feat: add nukki service

---
+++ services/nukki.py
@@ -0,0 +1,24 @@
+from utils.s3_utils import S3Service
+from PIL import Image
+from exceptions import ImageException
+from schemas import ImageResponse
+from core.settings import settings
+import numpy as np
+from rembg import remove
+
+class NukkiService:
+    def __init__(self):
+        self.s3_service = S3Service()
+        self.image_dir = settings.CARTOONIZATION_DIR
+
+    def convert(self, image: Image.Image, image_name: str):
+        converted_img = remove(np.array(image))
+
+        upload_result = self.s3_service.upload_image_to_s3(image=Image.fromarray(converted_img), image_dir=self.image_dir, image_name=image_name, format='PNG')
+
+        # s3 업로드 도중 에러 발생 시 ImageException 예외 발생
+        # TODO: exception handler 구현 필요
+        if upload_result['status'] == 'error':
+            raise ImageException(image_name=image_name, message=upload_result['message'])
+
+        return ImageResponse(image_url=upload_result['url'])
